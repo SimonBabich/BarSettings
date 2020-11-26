14a12f3f8163424dad2eee78d0a112e90429c1e3
SimonBabich <simon.babich1@gmail.com>
2020-11-26 19:26:38 +0300

    Initial Commit

---
+++ CoureAboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoureAboutMe
+//
+//  Created by BABICH Simon on 26.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
