0ff59302de3c37ef8de28db61ba4e8c8acacbbc6
SimonBabich <simon.babich1@gmail.com>
2021-05-23 20:16:26 +0300

    Жизненный цикл приложения. отображается только didFinishLaunchingWithOptions. Вместо applicationWillTerminate в дебагере видно событие: Terminated due to signal 9

--- CoureAboutMe/AppDelegate.swift
+++ CoureAboutMe/AppDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 import WatchConnectivity
 
+let fileNameAD = "AppDelegate.swift"
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate//, WCSessionDelegate
@@ -27,7 +28,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate//, WCSessionDelegate
 
 //    var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
 //        let vc = ViewController()
@@ -43,7 +47,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate//, WCSessionDelegate
 //            session.activateSession()
 //
 //        }
-        
+        print("01")
+        print(fileNameAD, #function)
         
         UITabBar.appearance().unselectedItemTintColor = .yellow
         return true
@@ -51,16 +56,47 @@ class AppDelegate: UIResponder, UIApplicationDelegate//, WCSessionDelegate
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
+//    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+//        // Called when a new scene session is being created.
+//        // Use this method to select a configuration to create the new scene with.
+//        print("001")
+//        print(fileNameAD, #function)
+//
+//        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+//    }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+//    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+//        // Called when the user discards a scene session.
+//        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+//        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+//        print("002")
+//        print(fileNameAD, #function)
+//    }
+    
+   
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        print("02")
+        print(fileNameAD, #function)
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        print("03")
+        print(fileNameAD, #function)
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        print("04")
+        print(fileNameAD, #function)
+    }
+    	
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        print("05")
+        print(fileNameAD, #function)
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        print("999")
+        print(fileNameAD, #function)
     }
     
 //    private func session(session: WCSession, didReciveMessage message: [String: AnyObject]){
